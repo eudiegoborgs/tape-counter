514c89164584107dadadea17c05051e2d2fb7404
Diego Borges <ferreirabdiego@gmail.com>
2020-09-17 11:10:58 -0300

    Update TapeServiceTest.java

--- src/softplan/TapeServiceTest.java
+++ src/softplan/TapeServiceTest.java
@@ -9,20 +9,18 @@ class TapeServiceTest {
 
     @Test
     public void case1() throws Exception {
-        service = new TapeService();
         int[] fileSizes = new int[]{8, 8, 2, 8, 2, 8, 8};
         int tapeSize = 10;
         Batch batch = new BatchImplementation(fileSizes, tapeSize);
-        int result = service.getMinimumTapeCount(batch);
+        int result = TapeService.getMinimumTapeCount(batch);
         assertEquals(result, 5);
     }
 
     public void case2() throws Exception {
-        service = new TapeService();
         int[] fileSizes = new int[]{20, 30, 50, 10, 15};
         int tapeSize = 100;
         Batch batch = new BatchImplementation(fileSizes, tapeSize);
-        int result = service.getMinimumTapeCount(batch);
+        int result = TapeService.getMinimumTapeCount(batch);
         assertEquals(result, 2);
     }
 }
